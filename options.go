a35628cec4d72c21249479ff37f3dc635d753bd4
Nico <nicholai.gerschewsky@clarilab.de>
2024-09-12 14:27:48 +0200

    refactor: get options

--- options.go
+++ options.go
@@ -3,6 +3,8 @@ package s3
 import (
 	"fmt"
 	"time"
+
+	"github.com/minio/minio-go/v7"
 )
 
 // Option is an option for the s3 client.
@@ -19,5 +21,31 @@ func WithHealthCheck(interval time.Duration) Option {
 		}
 
 		return nil
+	}
+}
+
+type getOption struct {
+	noContentDownload bool
+	clientGetOptions  minio.GetObjectOptions
+}
+
+// GetOption is an option for getting a file.
+type GetOption func(*getOption)
+
+// WithGetOptionDownloadContent enables or disables downloading the content of the file.
+//
+// Default is enabled.
+//
+// When disabled, the Content field of the returned File struct will be nil.
+func WithGetOptionDownloadContent(downloadContent bool) GetOption {
+	return func(o *getOption) {
+		o.noContentDownload = !downloadContent
+	}
+}
+
+// WithGetOptionClientGetOptions sets client options for the get request.
+func WithGetOptionClientGetOptions(options minio.GetObjectOptions) GetOption {
+	return func(o *getOption) {
+		o.clientGetOptions = options
 	}
 }
